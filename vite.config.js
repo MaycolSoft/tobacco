6ddb47328ef4721ffb5dd3a3e9d36df71952851c
mbaret <44105116+MaycolSoft@users.noreply.github.com>
2026-09-23 11:34:04 -0400

    fix(config): remove unused alias for utils in jsconfig and vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@components': path.resolve(__dirname, './src/components'),
       '@styles': path.resolve(__dirname, './src/styles'),
-      '@utils': path.resolve(__dirname, './src/utils'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@assets': path.resolve(__dirname, './src/assets')
     }
